983d8bc15cfee1fdd146cfabf9cb77708acf6056
rashidpkn <muhammed916rashid@gmail.com>
2022-03-11 02:00:38 +0530

    front-end login page is finish and user cart product page is started and some data fetching is needed for finishing the working of user login now I'm adjested with some dummy datas

--- frontend/pages/admin/index.jsx
+++ frontend/pages/admin/index.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Head from "next/head";
 import NavBar from '../components/NavBar';
+import { Form, Input, Label } from "reactstrap";
 
 export default function Adminlogin() {
   return (
-    <div className="container-fluid" >
+    <div className="container-fluid">
       <Head>
         <title>APDS - Admin Login</title>
       </Head>
       <NavBar></NavBar>
-      <div>Adminlogin</div>
+      <br />
+      <div className="loginform" style={{ width: "300px", margin: "auto" }}>
+        <Form>
+          <Input bsSize="sm" type="text" placeholder='Admin User'></Input><br />
+          <Input bsSize="sm" type="password" placeholder='Password'></Input>
+          <br />
+          <Input bsSize="sm" type="submit"></Input>
+        </Form>
+      </div>
     </div>
   );
 }
--- frontend/pages/user/index.jsx
+++ frontend/pages/user/index.jsx
@@ -1,15 +1,53 @@
 import React from 'react'
 import Head from "next/head";
 import NavBar from '../components/NavBar';
+import { Form, Input, Label } from 'reactstrap';
+import { useState } from 'react';
+
+
 
 export default function Userlogin() {
+  const [username, setusername] = useState("")
+  const [password, setpassword] = useState("")
+  const [signed,setsigned] = useState(false)
+  
+  const formHandler=(e)=>{
+    e.preventDefault();
+    //some fetching is needed
+    setsigned(true);
+  }
   return (
     <div className="container-fluid">
       <Head>
         <title>APDS - User Login</title>
       </Head>
       <NavBar></NavBar>
-      <div>Userlogin</div>
+      <br />
+      {!signed ?(
+        <div
+          className="loginform form-group"
+          style={{ width: "300px", margin: "auto" }}
+        >
+          <h3 style={{ textAlign: "center" }}>Hello {username}</h3>
+          <Form onSubmit={formHandler}>
+            <Input
+              bsSize="sm"
+              type="text"
+              placeholder="Ration Card No"
+              onChange={(e) => setusername(e.target.value)}
+            />
+            <br />
+            <Input
+              bsSize="sm"
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setpassword(e.target.value)}
+            />
+            <br />
+            <Input bsSize="sm" type="submit"></Input>
+          </Form>
+        </div>
+      ):(<h1>Welcome to cart please select your product</h1>)}
     </div>
   );
 }
