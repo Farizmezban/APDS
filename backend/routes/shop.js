3b37c6ae23735add6b95b831a92235938d700c3d
MUHAMMADMUFEEDK <mufeediqubal@gmail.com>
2022-04-06 11:24:01 +0530

    From Rashid SHop login is added

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get("/", function (req, res, next) {
   res.json({ users: [{ name: "Timmy" }] });
 });
+router.post('/',(req,res,next)=>{
+  if(req.body.username==='1010' && req.body.password==='12345')
+  {
+    res.json({signed:true})
+  }else{
+    res.json({signed:false})
+  }
+})
 
 module.exports = router;
